a9008af3f32fe2953f5bef53120e9038f0e69670
Mohit Lahoti <engtechno25@gmail.com>
2024-12-20 02:37:07 +0530

    Shift database from render to neon postgreSQL

--- amizone/settings.py
+++ amizone/settings.py
@@ -5,6 +5,7 @@ import os
 BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-f=25ye1ikyoesk)3w@a8==v2h7koo%lsgr81t7^dd$b2s5x97-'
 DATABASE_URL = "postgresql://amizone_db_user:UXv6v8OAtPOSbYrrtGlgZtkGSBEkvgpx@dpg-cti4ff8gph6c73d52fsg-a.oregon-postgres.render.com/amizone_db"
+DATABASE_URL = "postgresql://amizone_db_owner:Ej3zRcwM9INQ@ep-autumn-sound-a4t9rfto.us-east-1.aws.neon.tech/amizone_db"
 
 DEBUG = True
 
