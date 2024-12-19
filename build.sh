c3451618cf485c5fe8a67ca26aed5dc602e55dac
Mohit Lahoti <engtechno25@gmail.com>
2024-12-20 01:24:02 +0530

    adding python version in build.sh

--- build.sh
+++ build.sh
@@ -6,15 +6,15 @@ python3.12 pip install -U pip
 # Install dependencies
 
 echo "Installing project dependencies..."
-python -m pip install -r requirements.txt
+python3.12 -m pip install -r requirements.txt
 
 # Make migrations
 echo "Making migrations..."
-python manage.py makemigrations --noinput
-python manage.py migrate --noinput
+python3.12 manage.py makemigrations --noinput
+python3.12 manage.py migrate --noinput
 #Install whitenoise
 echo "Installing white noise..."
-python manage.py whitenoise
+python3.12 manage.py whitenoise
 # Collect staticfiles
 echo "Collect static..."
 python3.12 manage.py collectstatic --noinput --clear
