d5a0b093b9b914776e4a915ff28a284f5c7b68f5
Mohit Lahoti <engtechno25@gmail.com>
2024-12-20 01:31:25 +0530

    replace 3.12 to 3.10

--- build.sh
+++ build.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
 echo "Updating pip..."
-python3.12 pip install -U pip
+python3.10 pip install -U pip
 
 # Install dependencies
 
 echo "Installing project dependencies..."
-python3.12 -m pip install -r requirements.txt
+python3.10 -m pip install -r requirements.txt
 
 #Install whitenoise
 echo "Installing white noise..."
-python3.12 manage.py whitenoise
+python3.10 manage.py whitenoise
 # Collect staticfiles
 echo "Collect static..."
-python3.12 manage.py collectstatic --noinput --clear
+python3.10 manage.py collectstatic --noinput --clear
 
 echo "Build process completed!"```
