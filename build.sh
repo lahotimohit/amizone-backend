a4fc96e35c024a610a2a9b05b371cf60936fbf04
Mohit Lahoti <engtechno25@gmail.com>
2024-12-20 01:29:35 +0530

    Fixing issues

--- build.sh
+++ build.sh
@@ -3,6 +3,11 @@
 echo "Updating pip..."
 python3.12 pip install -U pip
 
+# Install dependencies
+
+echo "Installing project dependencies..."
+python3.12 -m pip install -r requirements.txt
+
 #Install whitenoise
 echo "Installing white noise..."
 python3.12 manage.py whitenoise
