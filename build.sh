c775519750b730d978b61d5383489b341216ff82
Mohit Lahoti <engtechno25@gmail.com>
2024-12-20 01:28:15 +0530

    Fixing issues

--- build.sh
+++ build.sh
@@ -3,15 +3,6 @@
 echo "Updating pip..."
 python3.12 pip install -U pip
 
-# Install dependencies
-
-echo "Installing project dependencies..."
-python3.12 -m pip install -r requirements.txt
-
-# Make migrations
-echo "Making migrations..."
-python3.12 manage.py makemigrations --noinput
-python3.12 manage.py migrate --noinput
 #Install whitenoise
 echo "Installing white noise..."
 python3.12 manage.py whitenoise
